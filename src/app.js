7b285dac755450a760edbd249856ef2fbc159050
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-02-18 23:29:31 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Kumar" });
-});
-
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Abhishek", lastName: "Kumar" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Response 1");
+    next();
+    // res.send("Route handler 1");
+  },
+  (req, res, next) => {
+    console.log("Response 2");
+    // res.send("Route handler 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Response 3");
+      // res.send("Route handler 3");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Response 4");
+      res.send("Route handler 4");
+    },
+  ],
+);
 
 app.listen(7777, () => {
   console.log("Server is successfully listening at port 7777...");
