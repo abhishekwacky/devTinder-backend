03544dbdb52d82d9fee11ed5a5ba28580eb92d42
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-02-18 10:20:19 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,34 +2,15 @@ const express = require("express");
 
 const app = express();
 
-// app.get("/hello", (req, res) => {
-//   res.send("Hello hello hello");
-// });
-
-app.use("/user", (req, res) => {
-  res.send("HAHAHAHAHAHAHA");
-});
-
 app.get("/user", (req, res) => {
+  console.log(req.query);
   res.send({ firstName: "Abhishek", lastName: "Kumar" });
 });
 
-app.post("/user", (req, res) => {
-  console.log("Send data to the database");
-  res.send("Data successfully saved to the database");
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Abhishek", lastName: "Kumar" });
 });
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully ");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from server");
-});
-
-// app.get("/", (req, res) => {
-//   res.send("Namaste from dashboard!");
-// });
 
 app.listen(7777, () => {
   console.log("Server is successfully listening at port 7777...");
