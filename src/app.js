177afff8b8d5c859c5ea7496d43d9abe7e87e805
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-02-22 16:03:17 +0530

    User-admin-middleware

--- src/app.js
+++ src/app.js
@@ -2,31 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Response 1");
-    next();
-    // res.send("Route handler 1");
-  },
-  (req, res, next) => {
-    console.log("Response 2");
-    // res.send("Route handler 2");
-    next();
-  },
-  [
-    (req, res, next) => {
-      console.log("Response 3");
-      // res.send("Route handler 3");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Response 4");
-      res.send("Route handler 4");
-    },
-  ],
-);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+//Handle all middleware for GET,POST request...
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully!!!");
+});
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("User Data sent");
+});
+
+app.get("/admin/getAllData", adminAuth, (req, res) => {
+  res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", adminAuth, (req, res) => {
+  res.send("Deleated a user");
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening at port 7777...");
 });
+
+//it will keep on going to middleware to middleware till it reaches a function which actually sends the response back and that function is known as request handler and all the function that is goes through in between it is known as middleware
+//Only job of express is to take the request and give the response back as soon as possible and it goes in order so to keep checking these functions because the main job of server is to respond back to the user
---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const adminAuth = (req, res, next) => {
+  console.log("Admin auth is getting checked!!");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("UnAuthorized Request");
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  console.log("User auth is getting checked!!");
+  // Dummy authorization logic
+  const token = req.headers["authorization"] || "xyz";
+  const isUserAuthorized = token === "xyz";
+  if (!isUserAuthorized) {
+    res.status(401).send("UnAuthorized Request");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  adminAuth,
+  userAuth,
+};
