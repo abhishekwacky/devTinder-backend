e2f34fc5376c0a1fd4d1adb0ecaba24987e33915
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-03-03 03:22:22 +0530

    Performed CRUD operations

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 
+//POST - user to the database
 app.post("/signup", async (req, res) => {
   // creating new instance of User Model
   const user = new User(req.body);
@@ -13,6 +14,51 @@ app.post("/signup", async (req, res) => {
     res.send("User added successfully");
   } catch (err) {
     res.status(404).send("Error saving user:", err.message);
+  }
+});
+
+//GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ email: userEmail });
+    res.send(user);
+    // const user = await User.find({ email: userEmail });
+    // if (user.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //     res.send(user);
+    // }
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//Delete user by id
+app.delete("/user", async (req, res) => {
+  const userid = req.body.id;
+  try {
+    const user = await User.findByIdAndDelete(userid);
+    res.send("user deleted successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//Feed API - GET/feed - get all the users from database
+app.get("/feed", (req, res) => {});
+
+//Update data of the user
+app.patch("/user", async (req, res) => {
+  const userid = req.body.id;
+  const data = req.body;
+  console.log(data);
+  try {
+    await User.findByIdAndUpdate({ _id: userid }, data);
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
   }
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://abhishek:AdnHFyix0OMs6izg@cluster0.4lwwzo5.mongodb.net/devTinder",
+    "mongodb+srv://abhishek:AmljxpnLj64nAoeX@cluster0.4lwwzo5.mongodb.net/devTinder",
   );
 };
 
